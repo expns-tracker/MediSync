772455a9227ed0a6f85bee6d25f102bf079e1f2c
Vlad Anghelache <anghelache.vlad.1@gmail.com>
2026-05-13 21:49:28 +0300

    Add logging

---
+++ backend/src/main/java/com/medisync/MediSync/aspect/LoggingAspect.java
@@ -0,0 +1,53 @@
+package com.medisync.MediSync.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Pointcut("within(com.medisync.MediSync.controller..*) || within(com.medisync.MediSync.service..*)")
+    public void applicationPackagePointcut() {
+    }
+
+    @Around("applicationPackagePointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (log.isDebugEnabled()) {
+            log.debug("Enter: {}.{}() with argument[s] = {}",
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(),
+                    Arrays.toString(joinPoint.getArgs()));
+        }
+
+        long start = System.currentTimeMillis();
+        try {
+            Object result = joinPoint.proceed();
+            long elapsedTime = System.currentTimeMillis() - start;
+
+            if (log.isDebugEnabled()) {
+                log.debug("Exit: {}.{}() with result = {}, Execution time = {} ms",
+                        joinPoint.getSignature().getDeclaringTypeName(),
+                        joinPoint.getSignature().getName(),
+                        result,
+                        elapsedTime);
+            }
+            return result;
+        } catch (IllegalArgumentException e) {
+            log.error("Illegal argument: {} in {}.{}()",
+                    Arrays.toString(joinPoint.getArgs()),
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName());
+            throw e;
+        }
+    }
+}
